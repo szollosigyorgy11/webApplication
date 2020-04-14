03a81a20c1f5ad5cd264db96dfc44f48838e5fb3
Szollosigy11 <33136248+Szollosigy11@users.noreply.github.com>
2020-04-14 18:51:01 +0200

    datas structure, thumbnails1

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+let datas = [];
 let images = ["/images/Budapest10.jpg","/images/Finale430.jpg","/images/Lausitzring12.jpg","/images/NBR10.jpg"];
+let titles = ["Budapest","Hochenheim-Final","Lausitzring","Nürburgring"];
+let discriptions = ["This race took place in the amazing Hungaroring, near to Budapest",0,0,0];
+let dataLoad = ((number, pic, tit, dis) => {
+    let data = {
+    id: number,
+    photo: pic,
+    title: tit,
+    discription: dis
+    };
+    return data;
+})
+
+for (let i = 0; i < images.length; i++) {
+    datas[i] = dataLoad(i,images[i], titles[i], discriptions[i]);
+}
+
 let pageCounter = 0;
 
 let loadPhoto = (photoNumber) => {
-    $("#photo").attr('src', images[photoNumber]);
+    $("#photo").attr('src', datas[photoNumber].photo);
+    $("#photo-title").text(datas[photoNumber].title);
+    $("#photo-description").text(datas[photoNumber].discription);
 }
 
 loadPhoto(pageCounter);
@@ -11,16 +30,25 @@ $(".previous").click(() => {
     if(pageCounter>0){
         pageCounter--;
     }else{
-        pageCounter = images.length - 1;
+        pageCounter = datas.length - 1;
     }
     loadPhoto(pageCounter);
 })
 
 $(".next").click(() => {
-    if(pageCounter<images.length-1){
+    if(pageCounter<datas.length-1){
         pageCounter++;
     }else{
         pageCounter = 0;
     }
     loadPhoto(pageCounter);
 })
+
+let insert =  0;
+for (let i = 0; i < datas.length; i++) {
+    insert = insert + '<div class="thumbnail" id="div'+i+'"> <img class="imgs" id="img'+i+'" src="'+datas[i].photo+'"> </div>';
+    
+}
+
+$(".menu").html(insert);
+
