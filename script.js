3bf8effbd3355f20ea080f92ac97c6e5431c481f
Szollosigy11 <33136248+Szollosigy11@users.noreply.github.com>
2020-04-15 23:22:32 +0200

    thumbnails desigen by Flora

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let datas = [];
-let images = ["/images/Budapest10.jpg","/images/Finale430.jpg","/images/Lausitzring12.jpg","/images/NBR10.jpg"];
+let images = ["/images/Budapest10.jpg","/images/Finale430.jpg","/images/Lausitzring12.jpg","/images/NBR10.jpg",0,0];
 let titles = ["Budapest","Hochenheim-Final","Lausitzring","Nürburgring"];
 let discriptions = ["This race took place in the amazing Hungaroring, near to Budapest",0,0,0];
 let dataLoad = ((number, pic, tit, dis) => {
@@ -18,10 +18,35 @@ for (let i = 0; i < images.length; i++) {
 
 let pageCounter = 0;
 
+let insert =  '';
+for (let i = 0; i < datas.length; i++) {
+    insert = insert + '<div class="thumbnail" id="div'+i+'"> <img class="imgs" data-number="'+i+'" src="'+datas[i].photo+'"> </div>';
+}
+
+$(".menu").html(insert);
+
+$(".thumbnail").css("width","50px");
+$(".thumbnail").css("height","50px");
+$(".thumbnail").css("background","white");
+$(".thumbnail").css("border","3px");
+$(".thumbnail").css("border-radius","3px");
+$(".thumbnail").css("margin","0px 2px");
+$(".thumbnail").css("position","relative");
+$(".thumbnail").hover(function(){$(this).css("bottom","20px")},function(){$(this).css('bottom',"0px")});
+$(".thumbnail").hover(function(){$(this).css("width","70px")},function(){$(this).css('width',"50px")});
+$(".thumbnail").hover(function(){$(this).css("height","70px")},function(){$(this).css('height',"50px")});
+$(".thumbnail").hover(function(){$(this).css("margin","0px")},function(){$(this).css('margin',"0px 2px")});
+
+$(".imgs").css("width","100%");
+$(".imgs").css("height","100%");
+$(".imgs").css("object-fit","cover");
+
 let loadPhoto = (photoNumber) => {
     $("#photo").attr('src', datas[photoNumber].photo);
     $("#photo-title").text(datas[photoNumber].title);
     $("#photo-description").text(datas[photoNumber].discription);
+    $(".thumbnail").css("box-shadow","none");
+    $("#div"+photoNumber).css("box-shadow","0px 5px 10px gray");
 }
 
 loadPhoto(pageCounter);
@@ -43,24 +68,6 @@ $(".next").click(() => {
     }
     loadPhoto(pageCounter);
 })
-
-let insert =  '';
-for (let i = 0; i < datas.length; i++) {
-    insert = insert + '<div class="thumbnail" id="div'+i+'"> <img class="imgs" data-number="'+i+'" src="'+datas[i].photo+'"> </div>';
-}
-
-$(".menu").html(insert);
-
-$(".thumbnail").css("width","100px");
-$(".thumbnail").css("height","50px");
-$(".thumbnail").css("padding","3px");
-$(".thumbnail").css("background","white");
-$(".thumbnail").css("border","3px");
-$(".thumbnail").css("border-radius","3px");
-$(".thumbnail").css("margin","0px 10px");
-
-$(".imgs").css("width","100%");
-$(".imgs").css("height","100%");
 
 $(".imgs").click(() => {
     pageCounter = $(event.target).attr("data-number");
